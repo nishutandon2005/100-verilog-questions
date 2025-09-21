61d6f1f312c9c86905ea1ee01024e83a4b4cbfa9
nishutandon2005 <156607101+nishutandon2005@users.noreply.github.com>
2025-09-21 18:32:23 +0530

    Create or_gate.v

---
+++ or_gate.v
@@ -0,0 +1,25 @@
+module or_gate(a,b,c);
+  input a,b;
+  output c;
+  assign c = a | b;
+endmodule
+
+module tb_or_gate;
+  reg a,b;
+  wire c;
+
+  or_gate uut (.a(a), .b(b), .c(c));
+
+  initial begin
+    $display("a  b  |  c");
+    $monitor("%b  %b  |  %b", a, b, c);
+
+    a=0; b=0; #10;
+    a=0; b=1; #10;
+    a=1; b=0; #10;
+    a=1; b=1; #10;
+    $finish;
+  end
+endmodule
+
+    
